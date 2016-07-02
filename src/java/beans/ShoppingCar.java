2ab1050f36f087cbd5ac27ebbc20e0005800bcf3
Jorge <Jorge@JorEA>
2016-07-01 20:26:49 -0600

    nuevo

--- src/java/beans/ShoppingCar.java
+++ src/java/beans/ShoppingCar.java
@@ -5,17 +5,63 @@
  */
 package beans;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  *
  * @author Jorge
  */
-public class ShoppingCar {
+public class ShoppingCar implements Serializable {
     
     private ArrayList<Purchase> carProducts = new ArrayList<>();
     private int subTotal;
     private int shippingCost;
     private int total;
+
+    public ShoppingCar() {
+    }
+
+    public ShoppingCar(int subTotal, int shippingCost, int total) {
+        this.subTotal = subTotal;
+        this.shippingCost = shippingCost;
+        this.total = total;
+    }
+
+    public ArrayList<Purchase> getCarProducts() {
+        return carProducts;
+    }
+
+    public void setCarProducts(ArrayList<Purchase> carProducts) {
+        this.carProducts = carProducts;
+    }
+
+    public int getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(int subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public int getShippingCost() {
+        return shippingCost;
+    }
+
+    public void setShippingCost(int shippingCost) {
+        this.shippingCost = shippingCost;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+    
+    
+    
+    
     
 }
